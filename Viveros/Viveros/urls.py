acb42638e7f9fe481f6d72d868c3fb78e8ef6df8
DimasHernandez <dimas.hernandez@utp.edu.co>
2020-11-12 02:17:06 -0500

    Aplicando plantilla bootstrap

--- Viveros/Viveros/urls.py
+++ Viveros/Viveros/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Vivero import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
