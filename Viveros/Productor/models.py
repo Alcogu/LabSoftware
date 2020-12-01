4a03f1f08aae63d42cbe2418932554af5c1c1c19
Alexander Correa Gutiérrez <30854317+Alcogu@users.noreply.github.com>
2020-12-01 10:33:46 -0500

    correccion db

--- Viveros/Productor/models.py
+++ Viveros/Productor/models.py
@@ -6,15 +6,12 @@ class Productor(models.Model):
     numero_documento = models.IntegerField()
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     numero_documento = models.IntegerField()
     
-=======
+
     numero_documento = models.IntegerField()
->>>>>>> Develop
-=======
-#<<<<<<< HEAD
+
     direccion = models.CharField(max_length=60)
     email = models.EmailField()
     telefono = models.CharField(max_length=10)
@@ -25,16 +22,3 @@ class Productor(models.Model):
     class Meta:
         verbose_name = "productor"
         verbose_name_plural = "productores"
-
-    
-
-    
-#=======
-#<<<<<<< HEAD
-#    numero_documento = models.IntegerField()
-#=======
-#    numero_documento = models.IntegerField()
-    
-#>>>>>>> dimas
-#>>>>>>> Develop
->>>>>>> dimas
--- Viveros/Viveros/settings.py
+++ Viveros/Viveros/settings.py
@@ -86,7 +86,14 @@ WSGI_APPLICATION = 'Viveros.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -97,7 +104,7 @@ DATABASES = {
         'DATABASE_PORT': '5432',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
