9b9c3d96c42fb43e99c65072796a9484d702d81c
Alexander Correa Gutiérrez <alexander092@hotmail.com>
2021-06-25 20:30:40 -0500

    errores

--- Viveros/Productor/models.py
+++ Viveros/Productor/models.py
@@ -6,26 +6,14 @@ class Productor(models.Model):
     numero_documento = models.IntegerField()
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     numero_documento = models.IntegerField()
-=======
+    numero_documento = models.IntegerField()
+
     numero_documento = models.IntegerField()
     
->>>>>>> dimas
-=======
-=======
     numero_documento = models.IntegerField()
-    
-=======
-    numero_documento = models.IntegerField()
->>>>>>> Develop
->>>>>>> Master
-=======
-#<<<<<<< HEAD
->>>>>>> dimas
+
     direccion = models.CharField(max_length=60)
     email = models.EmailField()
     telefono = models.CharField(max_length=10)
@@ -36,19 +24,4 @@ class Productor(models.Model):
     class Meta:
         verbose_name = "productor"
         verbose_name_plural = "productores"
-
-    
-
-    
-<<<<<<< HEAD
->>>>>>> dimas
-=======
-#=======
-#<<<<<<< HEAD
-#    numero_documento = models.IntegerField()
-#=======
-#    numero_documento = models.IntegerField()
-    
-#>>>>>>> dimas
-#>>>>>>> Develop
->>>>>>> dimas
+        
